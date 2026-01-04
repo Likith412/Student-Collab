ef02197f4951b623be783c940760250623f57f53
Likith Metikala <137510073+Likith412@users.noreply.github.com>
2026-01-04 22:20:58 +0530

    commit

--- frontend/src/components/ui/badge.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { cn } from "../../../lib/utils";
-import { badgeVariants } from "../../utils/badge-variants";
-
-function Badge({ className, variant, ...props }) {
-  return <div className={cn(badgeVariants({ variant }), className)} {...props} />;
-}
-
-export { Badge };
